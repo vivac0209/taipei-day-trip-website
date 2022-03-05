fd95f1b2690aea918ad7d8d496262705dcd7c649
viva chan <vivachan29@gmail.com>
2022-03-05 12:53:22 +0800

    change

--- static/index.js
+++ static/index.js
@@ -1,15 +1,1 @@
 
-
-let view = document.getElementById("viewData");
-let url = `http://127.0.0.1:5000/api/attractions`;
-
-fetch(url,{})
-        .then((response)=> {
-            return response.json();
-        })
-        .then((jsonData)=>{
-            console.log(jsonData);
-            let viewData = jsonData;
-            view.textContent = viewData;
-
-        })
