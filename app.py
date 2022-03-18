41d94dfb8bb53a912a48c90deebc3e6a088fc60e
vivac0209 <71447545+vivac0209@users.noreply.github.com>
2022-03-19 01:04:16 +0800

    Update app.py

--- app.py
+++ app.py
@@ -197,6 +197,4 @@ def thankyou():
 	return render_template("thankyou.html")
 
 
-# app.run(host='0.0.0.0', port=3000)
-if __name__ == '__main__':
-	app.run(port=3000)
+app.run(host='0.0.0.0', port=3000)
