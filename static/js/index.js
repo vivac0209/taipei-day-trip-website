1e2978e819da5ff1111708a8e8657d91b848c199
viva chan <vivachan29@gmail.com>
2022-03-26 23:05:10 +0800

    add new function

--- static/js/index.js
+++ static/js/index.js
@@ -146,15 +146,180 @@ searchBtn.addEventListener('click',() => {
     clearView.innerHTML='';
 })
 
+/*click登入 */
+
+let navLogin = document.getElementById("navlogin");
+
+navLogin.addEventListener('click',() => {
+    document.getElementById("login").style.display = "block";
+
+})
+
+/*註冊 */
+let gotoSignup = document.getElementById("gotoSignup");
+
+gotoSignup.addEventListener('click',() => {
+    document.getElementById("signup").style.display = "block";
+    document.getElementById("login").style.display = "none";
+    
+})
+/*註冊轉登入 */
+let gotoLogin = document.getElementById("gotoLogin");
+
+gotoLogin.addEventListener('click',() => {
+    document.getElementById("signup").style.display = "none";
+    document.getElementById("login").style.display = "block";
+})
+
+/*關閉登入視窗 */
+let closedBtn = document.getElementById("loginClose");
+closedBtn.addEventListener('click',() => {
+    document.getElementById("login").style.display = "none";
+   
+})
+/*關閉註冊視窗 */
+let signupClose = document.getElementById("signupClose");
+signupClose.addEventListener('click',() => {
+    
+    document.getElementById("signup").style.display = "none";
+})
+
+/* 收集登入資料*/
+let loginBtn = document.getElementById("loginbtn");
+
+loginBtn.addEventListener('click',function(){
+    let loginEmail = document.getElementById("loginEmail");
+    let loginPws = document.getElementById("loginPws");
+    let errorMsg = document.getElementById("loginMsg");
+    
+    if (loginEmail.value == "" | loginPws.value == ""){
+        errorMsg.textContent = "Email/Password 輸入空值";
+    }else{
+        loginAction(loginEmail,loginPws);
+    }
+
+})
+/*登入功能 */
+function loginAction(loginEmail,loginPws){
+    let loginUrl='/api/user';
+    // console.log("test");
+    // console.log(loginEmail.value,loginPws.value);
+    fetch(loginUrl,{
+        method: "PATCH",
+            headers: {"Content-Type": "application/json"},
+            body: JSON.stringify({
+            "email": loginEmail.value,
+            "password": loginPws.value
+        })
+    })
+    .then((res)=> {
+        return res.json();
+    })
+    .then((result)=>{
+        // console.log(result);
+        if (result["ok"]){
+            console.log("登入成功");
+            location.reload(); /*重新整理網頁 */
+            
+        }else{
+            let errorlogin = document.getElementById("loginMsg");
+            errorlogin.textContent = result["message"];
+            console.log(result["message"]);
+        }
+    })
+    
+}
+
+/* 收集註冊資料*/
+let signupBtn = document.getElementById("sbtn");
+let signupMsg = document.getElementById("signupMsg");
+
+signupBtn.addEventListener('click',function(){
+    let signupName = document.getElementById("signupName");
+    let signupEmail = document.getElementById("signupEmail");
+    let signupPws = document.getElementById("signupPws");
+    
+    if (signupName.value == "" | signupEmail.value == "" | signupPws.value == "" ){
+        signupMsg.textContent = "輸入空值";
+    }else{
+        signipAction(signupName,signupEmail,signupPws);
+    }
+
+})
+
+/*註冊功能 */
+function signipAction(signupName,signupEmail,signupPws){
+    let signnUrl='/api/user';
+    // console.log("test");
+    // console.log(loginEmail.value,loginPws.value);
+    fetch(signnUrl,{
+        method: "POST",
+            headers: {"Content-Type": "application/json"},
+            body: JSON.stringify({
+            "name": signupName.value,
+            "email": signupEmail.value,
+            "password": signupPws.value
+        })
+    })
+    .then((res)=> {
+        return res.json();
+    })
+    .then((result)=>{
+        console.log(result);
+        if (result["ok"]){
+            console.log("註冊成功");
+            signupMsg.textContent = "註冊成功";
+
+        }else{
+            signupMsg.textContent = result["message"];
+            console.log(result["message"]);
+        }
+    })
+    
+}
+
+/*確認登入狀態 */
+
+let checkUrl='/api/user';
+    
+let logout = document.getElementById("logout");
+let logignFrom = document.getElementById("login");
+
+fetch(checkUrl,{method: "GET"})
+    .then((res)=> {
+        return res.json();
+    })
+    .then((result)=>{
+        console.log("tset");
+        if (result["data"] != null){
+            navLogin.style.display = "none";
+            logout.style.display = "block";
+            logignFrom.style.display = "none";
+
+        }else{
+             navLogin.style.display = "block";
+            logout.style.display = "none";
+        }
+})
 
 
-/* <div class="viewBox">
-        <div class="pic">
-        <img class="img" src="https://www.travel.taipei/d_upload_ttn/sceneadmin/pic/11000848.jpg">
-        <div class="Title">新北投溫泉區</div>
-    </div>
-    <div class="viewText">
-        <div class="Mrt">新北投</div>
-        <div class="Category">養生溫泉</div>
-    </div>
-</div> */
+
+
+/*登出 */
+// let logoutBtn = document.getElementById("logout");
+
+// logoutBtn.addEventListener('click',() => {
+//     let logoutkUrl='/api/user';
+
+//     fetch(logoutkUrl,{method: "DELETE"})
+//         .then((res)=> {
+//             return res.json();
+//         })
+//         .then((result)=>{
+//             if (result["ok"]){
+//                 document.location.pathname='/'
+//                 // logoutBtn.style.display = "none";
+//                 // navLogin.style.display = "block";
+//             }
+//         })
+// })
